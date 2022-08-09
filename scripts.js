5ed4887ab2a6c8847d40f2c35c451c2fde4ce14e
Cameron <cameronpeck317@gmail.com>
2022-08-09 15:36:20 -0400

    add functionality for all buttons, clean up css sizing issues on numhistory box

--- scripts.js
+++ scripts.js
@@ -1,5 +1,32 @@
 // calculator functions
 
+// check for dividing by zero
+// prevent overflow on huge numbers
+// fix rounding on really really small numbers
+// add keyboard functionality
+
+// calculation variables
+let num1 = '';
+let num2 = '';
+let operator = '';
+let hasCalced = false;
+
+// button listeners
+const numberButtons = document.querySelectorAll('.num');
+const functionButtons = document.querySelectorAll('.func');
+const decimalButton = document.querySelector('#decimal');
+const percentageButton = document.querySelector('#percentage')
+const evaluateButton = document.querySelector('#evaluate');
+const allClearButton = document.querySelector('#all-clear');
+const backspaceButton = document.querySelector('#clear');
+const dataHistoryScreen = document.querySelector('.data-history');
+const dataInputScreen = document.querySelector('.data-input');
+
+Number.prototype.countDecimals = function () {
+    if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
+    return this.toString().split(".")[1].length || 0; 
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -34,77 +61,125 @@ function operate(operator, num1, num2) {
     }
 }
 
-let num1 = '';
-let num2 = '';
-let operator = '';
-let functionSet = false;
-// button listeners
-const numberButtons = document.querySelectorAll('.num')
-const functionButtons = document.querySelectorAll('.func')
-const evaluateButton = document.querySelector('#evaluate')
-const allClearButton = document.querySelector('#all-clear')
-const dataHistoryScreen = document.querySelector('.data-history')
-const dataInputScreen = document.querySelector('.data-input')
-
+function round(num) {
+    // if number of digits > 14
+        // calculate number of digits before and after the decimal
+        // calculate number of decimals to keep 
+        // round up the final decimal
+    // return the answer
+}
 
 numberButtons.forEach((numButton) => {
     numButton.addEventListener('click', () => {
-        // check if calcluator is ready to do math (is the function set?)
-        if (functionSet === false && num1 === '') {
+        // before we've done any calculation, inputs should be stored in num1
+        if (hasCalced === false) {
             num1 += numButton.id;
             dataInputScreen.textContent = `${num1}`;
-            console.log(`num1: ${num1}`)
+            console.log(`num1: ${num1}`);
         }
-        else if (functionSet === true) {
-            num2 += numButton.id
+        // after we've done a calculation, all future inputs will be num2
+        else if (hasCalced === true) {
+            num2 += numButton.id;
             dataInputScreen.textContent = `${num2}`;
             console.log(`num2: ${num2}`)
         }
-        
     });
 });
 
 functionButtons.forEach((funcButton) => {
     funcButton.addEventListener('click', () => {
+        // if num2 is present, evaluate the solution before continuing
+        if (num2) {
+            const firstNum = parseFloat(num1);
+            const secondNum = parseFloat(num2);
+            const operatedResult = operate(operator, firstNum, secondNum);
+            const roundedResult = operatedResult.toFixed(2)
+            dataInputScreen.textContent = roundedResult;
+            // get ready for next operation
+            num1 = `${roundedResult}`;
+            num2 = '';
+        }
         dataHistoryScreen.textContent = num1 + `${funcButton.id}`;
         // store the current operator the user wants to execute
-        operator = funcButton.id
-        // let the calculator know that it's time to do a math
-        functionSet = true;
-        console.log(`num1: ${num1}`)
+        operator = funcButton.id;
+        // let the calculator know that it's time to enter num2
+        hasCalced = true;
     });
+});
+
+decimalButton.addEventListener('click', () => {
+    if (num2 && !num2.includes('.')) {
+        num2 += '.';
+        dataInputScreen.textContent = num2;
+    }
+    else if (hasCalced === false & !num1.includes('.')) {
+        num1 += '.';
+        dataInputScreen.textContent = num1;
+    }
+});
+
+percentageButton.addEventListener('click', () => {
+    if (num2) {
+        const percentageNum2 = `${num2 * .01}`;
+        num2 = percentageNum2;
+        dataInputScreen.textContent = num2;
+    }
+    else if (hasCalced === false) {
+        const percentageNum1 = `${num1 * .01}`;
+        num1 = percentageNum1;
+        dataInputScreen.textContent = num1;
+    }
 });
 
 evaluateButton.addEventListener('click', () => {
     // change first and second nums from strings to integers
+    console.log(`operator:${operator} num2:${num2}`)
     if (num1 && num2) {
-        const firstNum = parseInt(num1)
-        const secondNum = parseInt(num2);
+        const firstNum = parseFloat(num1);
+        const secondNum = parseFloat(num2);
         const operatedResult = operate(operator, firstNum, secondNum);
-        dataInputScreen.textContent = operatedResult;
+        const roundedResult = operatedResult.toFixed(2)
+        dataInputScreen.textContent = roundedResult;
         // get ready for next operation
-        num1 = `${operatedResult}`;
-        num2 = ''
+        num1 = `${roundedResult}`;
+        num2 = '';
     }
-    
+});
+
+backspaceButton.addEventListener('click', () => {
+    // if num2 has data stored in it, backspace one index from num2
+    if (num2) {
+        const slicedString2 = num2.slice(0, num2.length - 1);
+        num2 = slicedString2;
+        dataInputScreen.textContent = num2;
+        console.log(num2);
+    }
+    // if data is stored in operator, remove last operator & update screen
+    else if (operator) {
+        operator = '';
+        dataHistoryScreen.textContent = num1;
+    }
+    // allow user to backspace on original inputs before calculations
+    else if (hasCalced === false) {
+        const slicedOriginal = num1.slice(0, num1.length - 1);
+        num1 = slicedOriginal
+        dataInputScreen.textContent = num1;
+        console.log(num1)
+    }
+    // only backspace up to the previous calculated number to prevent unwieldly calcs
+    else if (num1 && operator) {
+        const slicedString1 = num1.slice(0, num1.length - 1);
+        num1 = slicedString1;
+        dataInputScreen.textContent = num1;
+        console.log(num1);
+    }
 });
 
 allClearButton.addEventListener('click', () => {
     num1 = '';
     num2 = '';
     operator = '';
-    functionSet = false;
+    hasCalced = false;
     dataHistoryScreen.textContent = '';
     dataInputScreen.textContent = '';
-})
-
-
-// loop until either AC OR equal sign is pressed
-    // if AC pressed, clear content from data input and data history div
-    // reset current operation storage
-// on press -- modify text content of data input div
-    // listen for EITHER number press OR function press
-    // if number press, concatanate that number onto the end of data input div
-    // if function press, update history div with number and appropriate function sign
-        // unless nothing is stored, then do nothing
-    // if function is pressed instead of equal evaluate result
+});
